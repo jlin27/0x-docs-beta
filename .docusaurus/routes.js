23e10237db8f2a908d7ff1dbe8229cd721792bbe
Jessica Lin <jlin2700@gmail.com>
2023-03-25 20:16:15 -0700

    Remove Tutorials Basic and Extra

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -44,7 +44,7 @@ export default [
   },
   {
     path: '/',
-    component: ComponentCreator('/', '8b4'),
+    component: ComponentCreator('/', '2aa'),
     routes: [
       {
         path: '/',
@@ -407,18 +407,6 @@ export default [
         sidebar: "tutorialSidebar"
       },
       {
-        path: '/category/tutorial---basics',
-        component: ComponentCreator('/category/tutorial---basics', '72e'),
-        exact: true,
-        sidebar: "tutorialSidebar"
-      },
-      {
-        path: '/category/tutorial---extras',
-        component: ComponentCreator('/category/tutorial---extras', '3d7'),
-        exact: true,
-        sidebar: "tutorialSidebar"
-      },
-      {
         path: '/developer-resources/0x-legal-guide',
         component: ComponentCreator('/developer-resources/0x-legal-guide', '544'),
         exact: true,
@@ -679,54 +667,6 @@ export default [
       {
         path: '/protocol/docs/signatures',
         component: ComponentCreator('/protocol/docs/signatures', '374'),
-        exact: true,
-        sidebar: "tutorialSidebar"
-      },
-      {
-        path: '/tutorial-basics/congratulations',
-        component: ComponentCreator('/tutorial-basics/congratulations', '4b2'),
-        exact: true,
-        sidebar: "tutorialSidebar"
-      },
-      {
-        path: '/tutorial-basics/create-a-blog-post',
-        component: ComponentCreator('/tutorial-basics/create-a-blog-post', '93c'),
-        exact: true,
-        sidebar: "tutorialSidebar"
-      },
-      {
-        path: '/tutorial-basics/create-a-document',
-        component: ComponentCreator('/tutorial-basics/create-a-document', '58e'),
-        exact: true,
-        sidebar: "tutorialSidebar"
-      },
-      {
-        path: '/tutorial-basics/create-a-page',
-        component: ComponentCreator('/tutorial-basics/create-a-page', 'e6a'),
-        exact: true,
-        sidebar: "tutorialSidebar"
-      },
-      {
-        path: '/tutorial-basics/deploy-your-site',
-        component: ComponentCreator('/tutorial-basics/deploy-your-site', '317'),
-        exact: true,
-        sidebar: "tutorialSidebar"
-      },
-      {
-        path: '/tutorial-basics/markdown-features',
-        component: ComponentCreator('/tutorial-basics/markdown-features', '04e'),
-        exact: true,
-        sidebar: "tutorialSidebar"
-      },
-      {
-        path: '/tutorial-extras/manage-docs-versions',
-        component: ComponentCreator('/tutorial-extras/manage-docs-versions', '975'),
-        exact: true,
-        sidebar: "tutorialSidebar"
-      },
-      {
-        path: '/tutorial-extras/translate-your-site',
-        component: ComponentCreator('/tutorial-extras/translate-your-site', 'ac7'),
         exact: true,
         sidebar: "tutorialSidebar"
       }
